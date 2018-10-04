8db16e22f08e9a6a06150b9aeeb7083d1add7819
islamTaha12 <islamtaha2012@gmail.com>
2018-10-04 20:42:15 +0200

    update

--- testcases.py
+++ testcases.py
@@ -3,15 +3,20 @@ from base_test import BaseTest
 
 
 class TestS3Failures(BaseTest):
+
+    def tearDown(self):
+        super().tearDown()
+
     def test001_stop_parity_zdb(self):
         """
         - upload 10M in setupClass
-        - Stop n zdp, n <= parity
+        - Stop n zdb, n <= parity
         - Downlaod 10M file, should succeed
         - assert md5 checksum is matching
+        - Start n zdb
         """
         import ipdb; ipdb.set_trace()
-        self.logger.info(' [*] Stop {} zdb'.format((self.parity)))
+        self.logger.info(' Stop {} zdb'.format((self.parity)))
         md5_before = self.file_name
         self.s3.failures.zdb_down(count=self.parity)
 
@@ -20,4 +25,8 @@ class TestS3Failures(BaseTest):
 
         md5_after = self.download_file(file_name=self.file_name)
         self.assertEqual(md5_after, md5_before)
+
+        self.logger.info(' Start {} zdb'.format((self.parity)))
+        self.s3.failures.zdb_down(count=self.parity)
+
 
